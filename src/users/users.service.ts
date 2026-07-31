eb3f03e363287cc0cc30e2364a9d5ff3f242c44b
YYousabFouad <yosab.fouad@gmail.com>
2026-08-01 02:13:07 +0300

    feat: hash user passwords before saving

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-
+import * as bcrypt from 'bcrypt';
 import { User, UserDocument } from './schemas/user.schema';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
@@ -14,7 +14,12 @@ export class UsersService {
   ) {}
 
   async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    createUserDto.password = hashedPassword;
+
     const user = await this.userModel.create(createUserDto);
+
     return user;
   }
 
