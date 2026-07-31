f580e5d0eff2f9a9d2c76d6c5dc4839b6c6d282b
YYousabFouad <yosab.fouad@gmail.com>
2026-08-01 02:04:39 +0300

    feat: implement users CRUD

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,38 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { User, UserDocument } from './schemas/user.schema';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectModel(User.name)
+    private readonly userModel: Model<UserDocument>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const user = await this.userModel.create(createUserDto);
+    return user;
+  }
+
+  async findAll() {
+    return this.userModel.find();
+  }
+
+  async findOne(id: string) {
+    return this.userModel.findById(id);
+  }
+
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userModel.findByIdAndUpdate(id, updateUserDto, {
+      new: true,
+    });
+  }
+
+  async remove(id: string) {
+    return this.userModel.findByIdAndDelete(id);
+  }
+}
