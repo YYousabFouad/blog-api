55f52225e15f937cac331e3379514e028429ea86
YYousabFouad <yosab.fouad@gmail.com>
2026-08-01 15:38:04 +0300

    feat: configure JWT module

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -6,7 +7,16 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+
+    JwtModule.register({
+      secret: 'ITI_SECRET_KEY',
+      signOptions: {
+        expiresIn: '1d',
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
