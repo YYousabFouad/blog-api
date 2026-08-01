570420fa4fe2d49f782ac1a4dd8b360c0ebdd7bf
YYousabFouad <yosab.fouad@gmail.com>
2026-08-01 16:25:15 +0300

    feat: create groups module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { PostsModule } from './posts/posts.module';
 import { AuthModule } from './auth/auth.module';
+import { GroupsModule } from './groups/groups.module';
 
 @Module({
   imports: [
@@ -21,6 +22,8 @@ import { AuthModule } from './auth/auth.module';
     PostsModule,
 
     AuthModule,
+
+    GroupsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
