440cbb2e862a5b59cdf7bb8dcf061dddfff1fb86
YYousabFouad <yosab.fouad@gmail.com>
2026-08-01 16:14:07 +0300

    feat: add role field to user schema

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -1,6 +1,6 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
-
+import { Role } from '../../auth/enums/role.enum';
 export type UserDocument = HydratedDocument<User>;
 
 @Schema({
@@ -28,6 +28,13 @@ export class User {
 
   @Prop()
   image!: string;
+
+  @Prop({
+    type: String,
+    enum: Role,
+    default: Role.User,
+  })
+  role!: Role;
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
